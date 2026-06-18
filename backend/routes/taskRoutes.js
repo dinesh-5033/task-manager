d2f4cdb045f0422008045355138a408fa91f214b
Dinesh V <vsamy5421@gmail.com>
2026-06-18 10:26:19 +0530

    update all

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -18,7 +18,7 @@ router.post("/", async (req, res) => {
 router.get("/", async (req, res) => {
   try {
     const tasks = await Task.find();
-    res.json(tasks);
+    res.json(tasks);   // MUST be array
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
